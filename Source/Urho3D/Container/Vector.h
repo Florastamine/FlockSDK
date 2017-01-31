1451264fe5e62e624f654654dd5435648f9d3eec
Eugene Kozlov <nekoeuge@gmail.com>
2017-01-31 07:18:10 +0300

    Add [POD]Vector::IndexOf function.

--- Source/Urho3D/Container/Vector.h
+++ Source/Urho3D/Container/Vector.h
@@ -400,6 +400,12 @@ public:
         return it;
     }
 
+    /// Return index of value in vector, or size if not found.
+    unsigned IndexOf(const T& value) const
+    {
+        return Find(value) - Begin();
+    }
+
     /// Return whether contains a specific value.
     bool Contains(const T& value) const { return Find(value) != End(); }
 
@@ -976,6 +982,12 @@ public:
         while (it != End() && *it != value)
             ++it;
         return it;
+    }
+
+    /// Return index of value in vector, or size if not found.
+    unsigned IndexOf(const T& value) const
+    {
+        return Find(value) - Begin();
     }
 
     /// Return whether contains a specific value.
