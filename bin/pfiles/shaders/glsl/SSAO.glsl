cc4a3d59e5d320fd5c9ddf87bbe1681ae1815330
Florastamine <remilla.scarlet.94@gmail.com>
2017-07-26 12:08:29 +0700

    Improved SSAO quality and also added a parameter to customize SSAO "spreadness".

--- bin/pfiles/shaders/glsl/SSAO.glsl
+++ bin/pfiles/shaders/glsl/SSAO.glsl
@@ -6,7 +6,7 @@
 varying highp vec2 vScreenPos;
 
 #ifdef COMPILEVS
-    void VS() 
+    void VS()
     {
         mat4 modelMatrix = iModelMatrix;
         vec3 worldPos = GetWorldPos(modelMatrix);
@@ -17,20 +17,21 @@ varying highp vec2 vScreenPos;
 
 #ifdef COMPILEPS
     uniform highp vec2 cScreenSize;
+    uniform highp float cSSAOQuality;
     // Port from: https://github.com/jsj2008/Zombie-Blobs/blob/278e16229ccb77b2e11d788082b2ccebb9722ace/src/postproc.fs
     // see T M?ller, 1999: Efficiently building a matrix to rotate one vector to another
     mat3 rotateNormalVecToAnother(vec3 f, vec3 t)
     {
         vec3 v = cross(f, t);
         float c = dot(f, t);
-        float h = (1.0 - c) / (1.0 - c * c);
+        float h = c / (c * c);
         return mat3(c + h * v.x * v.x, h * v.x * v.y + v.z, h * v.x * v.z - v.y,
                     h * v.x * v.y - v.z, c + h * v.y * v.y, h * v.y * v.z + v.x,
                     h * v.x * v.z + v.y, h * v.y * v.z - v.x, c + h * v.z * v.z);
     }
 
     vec3 normal_from_depth(float depth, highp vec2 texcoords)
-    { 
+    {
         // One pixel: 0.001 = 1 / 1000 (pixels)
         const vec2 offset1 = vec2(0.0, 0.001);
         const vec2 offset2 = vec2(0.001, 0.0);
@@ -42,16 +43,15 @@ varying highp vec2 vScreenPos;
         normal.z = -normal.z;
         return normalize(normal);
     }
-
     void PS()
     {
-        const float aoStrength = 1.0;
+        const float aoStrength = 2.5;
         highp vec2 tx = vScreenPos;
         highp vec2 px = vec2(1.0 / cScreenSize.x, 1.0 / cScreenSize.y);
         float depth = DecodeDepth(texture2D(sEmissiveMap, vScreenPos).rgb);
         vec3  normal = normal_from_depth(depth, vScreenPos);
         // radius is in world space unit
-        const float radius = 1.0;
+        const float radius = 0.025;
         float zRange = radius / (cFarClipPS - cNearClipPS);
         // calculate inverse matrix of the normal by rotate it to identity
         mat3 InverseNormalMatrix = rotateNormalVecToAnother(normal, vec3(0.0, 0.0, 1.0));
@@ -60,21 +60,25 @@ varying highp vec2 vScreenPos;
         // http://www.cs.utah.edu/~loos/publications/vo/vo.pdf
         float hemi = 0.0;
         float maxi = 0.0;
-        for (int x = -2; x <= 2; ++x) {
-            for (int y = -2; y <= 2; ++y) {
+        float screenSample = 1.5;
+        int quality_lower = int(cSSAOQuality) * -1;
+        int quality_upper = quality_lower * -1;
+        for (int x = quality_lower; x <= quality_upper; ++x)
+        {
+            for (int y = quality_lower; y <= quality_upper; ++y)
+            {
                 // make virtual sphere of unit volume, more closer to center, more ambient occlusion contributions
-                float rx = 0.3 * float(x);
-                float ry = 0.3 * float(y);
+                float rx = 0.03 * float(x);
+                float ry = 0.03 * float(y);
                 float rz = sqrt(1.0 - rx * rx - ry * ry);
                 highp vec3 screenCoord = vec3(float(x) * px.x, float(y) * px.y, 0.0);
                 // 0.25 times smaller when farest, 5.0 times bigger when nearest.
-                highp vec2 coord = tx + (5.0 - 4.75 * depth) * screenCoord.xy;
+                highp vec2 coord = tx + (screenSample * screenCoord.xy);
                 // fetch depth from texture
                 screenCoord.z = DecodeDepth(texture2D(sEmissiveMap, coord).rgb);
                 // move to origin
                 screenCoord.z -= depth;
                 // ignore occluders which are too far away
-                if (screenCoord.z < -zRange) continue;
 
                 // Transform to normal-oriented hemisphere space
                 highp vec3 localCoord = InverseNormalMatrix * screenCoord;
@@ -89,6 +93,8 @@ varying highp vec2 vScreenPos;
         }
 
         float ao = clamp(hemi / maxi, 0.0, 1.0);
-        gl_FragColor = vec4(texture2D(sDiffMap, vScreenPos).rgb * ao, 1.0);
+        ao = mix(ao, 1.0, 0.4);
+        vec3 finalColor = texture2D(sDiffMap, vScreenPos).rgb * ao;
+        gl_FragColor = vec4(finalColor, 1.0);
     }
-#endif 
+#endif
