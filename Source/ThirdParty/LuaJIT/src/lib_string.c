e4ed084cb5f2ff75f864a8a07782cce1ed9fbcc8
Florastamine <remilla.scarlet.94@gmail.com>
2017-10-06 17:59:39 +0700

    Proper license notices on Premake5-related code.

--- Source/ThirdParty/LuaJIT/src/lib_string.c
+++ Source/ThirdParty/LuaJIT/src/lib_string.c
@@ -84,26 +84,37 @@ LJLIB_ASM(string_sub)		LJLIB_REC(string_range 1)
   return FFH_RETRY;
 }
 
-/* 
- * Original function (string_endswith()) from the Premake project (https://premake.github.io), 
- * modified and adapted into LuaJIT by Florastamine. (https://github.com/Florastamine) 
- */
 /*
-PREMAKE
-A build configuration tool
+PREMAKE 5 (core) license
+---------------
 
- Copyright (C) 2002-2013 by Jason Perkins
- Distributed under the terms of the BSD License, see LICENSE.txt
+Copyright (c) 2003-2016 Jason Perkins and individual contributors.
+All rights reserved.
 
- The Lua language and runtime library is (C) TeCGraf, PUC-Rio.
- See their website at http://www.lua.org/
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
 
+  1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
 
- See the file BUILD.txt for instructions on building Premake.
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
 
+  3. Neither the name of Premake nor the names of its contributors may be
+     used to endorse or promote products derived from this software without
+     specific prior written permission.
 
- For questions, comments, or more information, visit the project
- website at http://industriousone.com/premake
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 LJLIB_CF(string_ends_with) 
 {
